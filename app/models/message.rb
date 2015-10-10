a33f66e8eab34435ea6e7aa605fe96ba1d799bbf
Ajwah <johnson786.kevin+git@gmail.com>
2015-10-10 21:13:16 +0000

    Add validation to check format of url only if url has been submitted.

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,6 @@ class Message < ActiveRecord::Base
 
   validates :content, presence: true, length: { maximum: 140 }
   validates :author, presence: true, length: { maximum: 25 }
+  validates_format_of :url, :with => /\A((http|https):\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix, :allow_blank => true
 
 end
