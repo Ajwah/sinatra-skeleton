031739c1428cc687ac489da6d786562945de7196
Ajwah <johnson786.kevin+git@gmail.com>
2015-10-10 18:21:13 +0000

    Remove title and migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009222616) do
+ActiveRecord::Schema.define(version: 20151010181404) do
 
   create_table "messages", force: :cascade do |t|
-    t.string "title"
     t.string "author"
     t.string "content"
   end
